3192f44ea19fe7806aed809c0150e26ff343ea27
dpost <dpost@DESKTOP-7V0E006>
2021-01-17 13:07:33 +0300

    Добавьте файлы проекта.

---
+++ ATS_EPAM_HOMETASK_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATS_EPAM_HOMETASK_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
